d860c07ef0050993fed09bff9a18ce645251f855
Olya\Olga <olya.kurzeneva@mail.ru>
2018-04-04 06:43:15 +0300

    Code

---
+++ Code/HeadLookWalk.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+
+public class HeadLookWalk : MonoBehaviour {
+    public float velocity = 0.7f;
+    public bool isWalking = false;
+    private Clicker clicker = new Clicker();
+    private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+        
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ax = Input.GetAxis("Button_A");
+        //if (ax == -0.1861143)
+        //if (Input.GetAxis("mouse 0") != 0)
+        //if (clicker.clicked())
+        //if (Input.GetKeyDown("Button_A"))
+        //if (CnInputManager.GetButton("Button_A"))
+        //{
+        //    //isWalking = !isWalking;
+        //}
+        if(CnInputManager.GetButton("Button_Y"))
+        {
+            controller.SimpleMove(Camera.main.transform.forward * velocity);
+        }
+        if (CnInputManager.GetButton("Button_A"))
+        {
+            controller.SimpleMove(Camera.main.transform.forward * -velocity);
+        }
+        if (CnInputManager.GetButton("Button_X"))
+        {
+            controller.SimpleMove(Camera.main.transform.right * -velocity);
+        }
+        if (CnInputManager.GetButton("Button_B"))
+        {
+            controller.SimpleMove(Camera.main.transform.right * velocity);
+        }
+    }
+}
---
+++ Code/pick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pick : MonoBehaviour
+{
+    public int parts = 0;
+
+    void OnCollisionEnter(Collision myCollision)
+    {
+        if (myCollision.gameObject.name == "f1" || myCollision.gameObject.name == "f")
+        {
+            // Обращаемся к имени объекта с которым столкнулись
+            Destroy(myCollision.gameObject);
+            Debug.Log("Good");
+            parts++;
+        }
+
+        if (myCollision.gameObject.name == "door2" && parts == 2)
+        {
+            // Обращаемся к имени объекта с которым столкнулись
+            Destroy(myCollision.gameObject.GetComponent<BoxCollider>());
+        }
+    }
+}
+//    void OnTriggerEnter(Collider myCollision)
+//    {
+
+//        if (myCollision.gameObject.name == "f1" || myCollision.gameObject.name == "f")
+//        {
+//        //    // Обращаемся к имени объекта с которым столкнулись
+//            //Destroy(gameObject);
+//            Debug.Log("Good");
+//            parts++;
+//        }
+
+//        //if (myCollision.gameObject.name == "door2" && parts == 2)
+//        //{
+//        //    // Обращаемся к имени объекта с которым столкнулись
+//        //    Destroy(myCollision.gameObject.GetComponent<BoxCollider>());
+//        //}
+//    }
+//}
